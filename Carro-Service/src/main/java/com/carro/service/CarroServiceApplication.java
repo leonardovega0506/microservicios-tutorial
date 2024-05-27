e39880147775e655d01d1556d54a569a716852d2
Leonardo Vega Pérez <leo.vega0506@gmail.com>
2024-05-27 12:34:13 -0600

    Eureka

--- Carro-Service/src/main/java/com/carro/service/CarroServiceApplication.java
+++ Carro-Service/src/main/java/com/carro/service/CarroServiceApplication.java
@@ -2,8 +2,11 @@ package com.carro.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CarroServiceApplication {
 
 	public static void main(String[] args) {
